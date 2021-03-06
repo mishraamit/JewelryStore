ecf45933bfe3ea30933754c90b125f7ec9d98e44
Amit Mishra <AmMishra@gep.com>
2021-03-06 21:03:24 +0530

    Sample empty project

---
+++ JewelryStoreAssignment/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+    alert("Login init");
+  }
+
+  Login(){
+    alert("Logged In");
+  }
+
+}
